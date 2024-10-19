94269a4d79205b52c06eee16876d4c4b3bdcfeb9
Kevin Aka <kevinnaka.kna@gmail.com>
2024-10-19 12:47:25 -0400

    Our Current Nav bar

---
+++ neip-app/src/app/pages/Homepage.tsx
@@ -0,0 +1,10 @@
+
+const Homepage = () => {
+    return (
+        <>
+            <p>1-Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum aspernatur libero amet harum similique repellat tempora! Nesciunt quam aperiam ratione eius molestias temporibus autem eveniet? Quasi impedit quos iure architecto!</p>
+        </>
+    )
+}
+
+export default Homepage;
