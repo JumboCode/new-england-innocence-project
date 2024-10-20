8e3897d7293e4b78e29bbee6740eb6a328e9cb5b
siarasmall <41011312+siarasmall@users.noreply.github.com>
2024-10-20 15:30:54 -0400

    Moved AuthBox out of onboarding dir

--- neip-app/src/app/page.tsx
+++ neip-app/src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 
 import MyButton from "./components/onboarding/Button";
-import AuthBox from "./components/onboarding/AuthBox";
+import AuthBox from "./components/AuthBox";
 
 export default function Home() {
   return (
+    <AuthBox></AuthBox>
     // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
     //   <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
     //     <MyButton label="Click to get the weather" />
