aed5781a052e7e410f9108b3be0b3d82ef22e626
Henri Comer <henrijcomer@gmail.com>
2024-11-02 13:38:31 -0400

    Co-authored-by: birdy1987 <birdy1987@users.noreply.github.com> Co-authored-by: dmehta05 <dmehta05@users.noreply.github.com>

--- neip-app/src/pages/Signup.tsx
+++ neip-app/src/pages/Signup.tsx
@@ -11,6 +11,29 @@ const Signup: React.FC = () => {
 }
 
 const SignupContent = () => {
+
+    const [userId, setUserId] = useState('');
+    const [email, setEmail] = useState('');
+    const [position, setPosition] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSignup = async () => {
+        const response = await fetch('/api/auth/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email,
+                password,
+            }),
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to create account');
+        }
+    }
+
     return (
         <div>
             <div>
@@ -49,7 +72,7 @@ const SignupContent = () => {
                 width: '375px',
                 padding: '27px',
             }}>
-                <AuthEntryBox placeholder="user ID" type="text" />
+                <AuthEntryBox placeholder="user ID" type="text" onChange={(e) => setUserId(e.target.value)}/>
             </div >
 
             {/* email input */}
@@ -61,7 +84,7 @@ const SignupContent = () => {
                 width: '375px',
                 padding: '27px',
             }}>
-                <AuthEntryBox placeholder="email" type="email" />
+                <AuthEntryBox placeholder="email" type="email" onChange={(e) => setEmail(e.target.value)} />
             </div>
 
             {/* position input */}
@@ -73,7 +96,7 @@ const SignupContent = () => {
                 width: '375px',
                 padding: '27px',
             }}>
-                <AuthEntryBox placeholder="new user position" type="text" />
+                <AuthEntryBox placeholder="new user position" type="text" onChange={(e) => setPosition(e.target.value)}/>
             </div>
 
             {/* password input */}
@@ -85,7 +108,7 @@ const SignupContent = () => {
                 width: '375px',
                 padding: '27px',
             }}>
-                <AuthEntryBox placeholder="password" type="password" /> 
+                <AuthEntryBox placeholder="password" type="password" onChange={(e) => setPassword(e.target.value)}/> 
             </div>
 
             {/* sign up button */}
@@ -95,7 +118,7 @@ const SignupContent = () => {
                 alignItems: 'center',
                 padding: '10px',
             }}>
-                 <AuthButton color='#43b4ef' filled={true} text='Sign Up' href="/signupConfirmation"/>
+                 <AuthButton color='#43b4ef' filled={true} text='Sign Up' href="/signupConfirmation" onClick={handleSignup}/>
             </div>
         </div>
     );
