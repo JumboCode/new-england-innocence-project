5747beefe2e28e5143f80db6a476bbcabcd1f709
Dhruvii Mehta <dhruviimehta@Dhruviis-MacBook-Air.local>
2024-12-08 22:12:04 -0500

    personal-info-and-case-info

--- neip-app/src/components/LabelAndEntry.tsx
+++ neip-app/src/components/LabelAndEntry.tsx
@@ -6,64 +6,49 @@ interface LabelAndEntryProps {
    placeholder: string;
    width?: string;
    height?: string;
-   //borderRadius: string;
-   //color: boolean;
+   borderRadius?: string;
 }
 
-const LabelAndEntry: React.FC<LabelAndEntryProps> = ({label, placeholder, width, height}) => {
-    // const LabelAndEntryStyle: React.CSSProperties = {
-        // label: label,
-        // placeholder: placeholder,
-        // labelColor: labelColor,
-        // placeholderColor: placeholderColor,
-    //     border: '2px',
-    //     borderRadius: '20px',
-    //     alignItems: 'left',
-    //     fontFamily: 'Inter, sans-serif',
-    //     fontSize: '14px',
-    //     //color: color ? 'white' : 'grey',
-    //     borderColor: '#CCDDF8',
-    //     height: height, // Use the height prop
-    //     width: width, // Use the width prop
-    // }
+const LabelAndEntry: React.FC<LabelAndEntryProps> = ({label, placeholder, width, height, borderRadius}) => {
     const containerStyle: React.CSSProperties = {
         display: 'flex',
         flexDirection: 'column',
-        width: width,
-        alignItems: 'flex-start'
+       // width: width,
+        alignItems: 'flex-start',
     };
 
     const inputStyle: React.CSSProperties = {
         color: 'black',
         fontSize: '13px',
         fontFamily: 'Inter, sans-serif',
-        border: '1px solid #D0D5DD',
-        borderRadius: '20px',
-        padding: '10px 16px',
+        border: '1px solid #CCDDF8',
+        //borderRadius: '20px',
+        borderRadius: borderRadius,
+        padding: '10px 10px',
+        width: width,
+        height: height,
     };
 
     const labelStyle: React.CSSProperties = {
         color: '#667085',
         fontSize: '13px',
         fontFamily: 'Inter, sans-serif',
+        padding: '10px 2px',
     };
-    const placeHolderstyle: React.CSSProperties = {
-        color: '#CCDDF8',
-        fontSize: '15px',
-        fontFamily: 'Inter, sans-serif',
-    };
+    // const placeHolderstyle: React.CSSProperties = {
+    //     color: '#CCDDF8',
+    //     fontSize: '15px',
+    //     fontFamily: 'Inter, sans-serif',
+    // };
 
     return (
         <div style={containerStyle}>
             <label style={labelStyle}>{label}</label>
-            <input type= 'text'
-            placeholder={placeholder}
-            style={inputStyle}>
-            </input>
-            
-
+                <input type= 'text'
+                    placeholder={placeholder}
+                    style={inputStyle}>
+                </input>
         </div>
-
     )
 };
 
