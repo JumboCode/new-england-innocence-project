23182c8d8a2a8703112c1b202b6445dfe3f29c14
siarasmall <41011312+siarasmall@users.noreply.github.com>
2025-01-09 14:56:29 -0500

    Rm temp rendering

--- neip-app/src/pages/index.tsx
+++ neip-app/src/pages/index.tsx
@@ -1,9 +1,7 @@
 import NavBar from '../components/NavBar';
 import HomePage from './HomePage';
-import DropdownAndTags from '../components/DropdownAndTags';
 
 const Homepage = () => {
-  const options = ['Detective 1', 'Detective 2', 'Detective 3'];
   return (
     <div className="flex h-screen">
       <div>
