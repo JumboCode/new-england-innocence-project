f3e4447aba4e891f88ea6bcba598628410e74ebb
Henri Comer <henrijcomer@gmail.com>
2025-01-29 12:23:19 -0500

    Co-authored-by: dmehta05 <dmehta05@users.noreply.github.com>

--- neip-app/src/pages/api/filter/filterByInt.ts
+++ neip-app/src/pages/api/filter/filterByInt.ts
@@ -1,1 +1,38 @@
-// TODO
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const {
+        value,
+        field,
+        constraint,
+        model
+    } = req.body
+
+    if (
+        !value || typeof value !== 'number' ||
+        !field || typeof field !== 'string' ||
+        !['<', '<=', '>', '>=', '='].includes(constraint)
+    ) {
+        return res.status(400).json({ error: 'Missing required fields' });
+    }
+
+    try {
+        const query = `
+            SELECT id
+            FROM "${model}"
+            WHERE "${field}" ${constraint} ${value}
+        `;
+
+        const exonerees = await prisma.$queryRawUnsafe<{ id: number }[]>(query);
+
+        const ids = exonerees.map((result)  => result.id);
+
+        return res.status(200).json({exonerees: ids});
+
+    } catch (error) {
+        return res.status(400).json({ error: 'Failed to get IDs'});
+    }
+}
