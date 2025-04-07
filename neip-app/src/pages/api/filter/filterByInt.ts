b7dc7cb742ff9573735f22cbeb26b4d81adbe386
leann-dinh <145501652+ldinh67@users.noreply.github.com>
2025-04-06 20:35:05 -0400

    Updates

--- neip-app/src/pages/api/filter/filterByInt.ts
+++ neip-app/src/pages/api/filter/filterByInt.ts
@@ -1,36 +1,37 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from '../../../utils/database/connectToDb'
 
-export default async function handler (
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { value, field, constraint, model } = req.body
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const {
+        value,
+        field,
+        constraint,
+        model
+    } = req.body
 
-//   if (
-//     // !value || typeof value !== 'number' ||
-//     !field ||
-//     typeof field !== 'string' ||
-//     !['<', '<=', '>', '>=', '='].includes(constraint)
-//   ) {
-//     return res.status(400).json({ error: 'Missing required fields' })
-//   }
+    if (
+        !value || typeof value !== 'number' ||
+        !field || typeof field !== 'string' ||
+        !['<', '<=', '>', '>=', '='].includes(constraint)
+    ) {
+        return res.status(400).json({ error: 'Missing required fields' });
+    }
 
-  try {
-    const int_val = parseInt(value)
-    const query = `
+    try {
+        const query = `
             SELECT id
             FROM "${model}"
-            WHERE "${field}" ${constraint} ${int_val}
-        `
+            WHERE "${field}" ${constraint} ${value}
+        `;
 
-    const exonerees = await prisma.$queryRawUnsafe<{ id: number }[]>(query)
+        const exonerees = await prisma.$queryRawUnsafe<{ id: number }[]>(query);
 
-    const ids = exonerees.map(result => result.id)
+        const ids = exonerees.map((result)  => result.id);
 
-    return res.status(200).json({ exonerees: ids })
-  } catch (error) {
-    console.log(error)
-    return res.status(400).json({ error: 'Failed to get IDs' })
-  }
+        return res.status(200).json({exonerees: ids});
+
+    } catch (error) {
+        return res.status(400).json({ error: 'Failed to get IDs'});
+        console.log(error);
+    }
 }
